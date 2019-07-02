299f8a175370eaf00b7cc5c1610278850c4a21e5
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-07-02 22:25:45 +0100

    Add nameserver config

--- hosts/m1vm/configuration.nix
+++ hosts/m1vm/configuration.nix
@@ -27,6 +27,7 @@
     prefixLength = 24;
   }];
   networking.defaultGateway = "192.168.0.254";
+  networking.nameservers = ["192.168.0.4"];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
