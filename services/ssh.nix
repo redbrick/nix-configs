1143b9ce37edfb131bbd5416545bfc61bc709a8e
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-08-06 21:55:25 +0100

    Remove some ssh keys

--- services/ssh.nix
+++ services/ssh.nix
@@ -3,9 +3,6 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINnVLSh0OStxZTkXE6oGgwfFvsbvN6bFPlVfDYOwtnzn lucas@oatfield"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPR+OTAIYr02f/WKQSXo7zYy9tkuAHYpy0ajqY6aJ7Nk m1cr0man@redbrick.dcu.ie"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMqatoGLGjnuRUhMV5gXcNDhNs3pm/escyEXn8s9Nft4 lucas@sentinel-prime"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJKV6EiRMZHmXI4t9H5awE3fSTU7apHsTLF2iHZQFSgy lucas@dryarch"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOCOutDnpAaDvY70HU/PBVsHwFENuNTgMjE06N8lNDqe root@nixos"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDYKnYP4Mmyk4wQE7J6Tyr27XToKtxAhXBZr5HkEXiFq root@gelandewagen"
   ];
 }
