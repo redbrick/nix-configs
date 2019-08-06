249eae94762620c161591632291b411c19bc9621
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-08-06 21:52:37 +0100

    Add Daedalus and Icarus configs

---
+++ hosts/daedalus/configuration.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../common/sysconfig.nix
+    ../../services/ssh.nix
+  ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09";
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/sda";
+
+  networking.hostName = "daedalus";
+  networking.interfaces.eno1.ipv4.addresses = [{
+    address = "192.168.0.50";
+    prefixLength = 24;
+  }];
+  networking.defaultGateway = "192.168.0.254";
+  networking.nameservers = ["192.168.0.4"];
+}
