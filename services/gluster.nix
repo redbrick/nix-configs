9c8f66a64ecb44ec47baab981d431a9d99469e40
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-08-15 23:20:04 +0100

    Open gluster ports

--- services/gluster.nix
+++ services/gluster.nix
@@ -4,4 +4,16 @@
     # TODO change in prod
     logLevel = "INFO";
   };
+
+  # For each brick open port 49152 + brick_num
+  # Our nodes have 3 bricks
+  # Opening a heap of ports to make things easier in the future
+  # Max 15 if the powervault was to have 1 brick per drive
+  networking.firewall.allowedTCPPorts = [
+    111 24007 24008 38465 38466 38467
+    49152 49153 49154 49155 49156 49157
+    49158 49159 49160 49161 49162 49163
+    49164 49165 49166
+  ];
+  networking.firewall.allowedUDPPorts = [ 111 ];
 }
