1348d023909f407e76ee0ff19f7d6e228e2af05d
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-08-18 17:00:09 +0100

    Explicitly add common.tld to search

--- common/sysconfig.nix
+++ common/sysconfig.nix
@@ -17,7 +17,7 @@ in {
 
   # Use Redbrick DNS and HTTP proxy
   networking.domain = common.tld;
-  networking.search = [ "internal" ]; # Networking.domain is appended by Nix
+  networking.search = [ "internal" common.tld ];
   networking.nameservers = ["192.168.0.4"];
   networking.proxy.default = "http://proxy.internal:3128/";
   networking.proxy.noProxy = "127.0.0.1,localhost,*.internal";
