f7751473f90776d9ca89609aa51ffd7a7fa7522b
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-08-24 13:51:31 +0100

    Add slave zone for .internal

--- services/dns/default.nix
+++ services/dns/default.nix
@@ -2,6 +2,14 @@
   services.bind = {
     enable = true;
 
+    cacheNetworks = [
+      "127.0.0.0/24"
+      "192.168.0.0/24"
+      "192.168.1.0/24"
+      "136.206.15.0/24"
+      "136.206.16.0/24"
+    ];
+
     zones = [
       {
         # Not using common.tld here becaue we actually want to configure
@@ -14,6 +22,11 @@
         file = ./redbricktest.ml.rr;
         master = true;
         name = "15.206.136.in-addr.arpa";
+      }
+      {
+        masters = [ "192.168.0.4" ];
+        master = false;
+        name = "internal";
       }
     ];
   };
