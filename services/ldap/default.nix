dc7a9b02c28a578230686e6efe633529e0610887
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-09-07 01:20:12 +0100

    Fix indentation issues

--- services/ldap/default.nix
+++ services/ldap/default.nix
@@ -2,53 +2,53 @@
 let
   rootpwFile = "/var/lib/private/ldap.secret";
 in {
-	services.openldap = {
+  services.openldap = {
     inherit rootpwFile;
-		enable = true;
+    enable = true;
     suffix = "o=redbrick";
     rootdn = "cn=root,ou=ldap,o=redbrick";
     database = "hdb";
-		extraConfig = ''
+    extraConfig = ''
       include ${./schema/common.schema}
       include ${./schema/system.schema}
       include ${./schema/userdb.schema}
 
-			backend hdb
-			lastmod on
+      backend hdb
+      lastmod on
 
-			cachesize 100000
+      cachesize 100000
 
-			sizelimit unlimited
+      sizelimit unlimited
 
-			# ACLs
-			access to dn.children="ou=2002,ou=accounts,o=redbrick"
+      # ACLs
+      access to dn.children="ou=2002,ou=accounts,o=redbrick"
         by dn.regex="cn=root,ou=ldap,o=redbrick" write
         by * none
 
-			access to dn.children="ou=accounts,o=redbrick" attrs=cn
-        by dn.regex="cn=root,ou=ldap,o=redbrick" write
-        by self read
-        by * none
-
-			access to attrs=yearsPaid,year,course,id,newbie,altmail
+      access to dn.children="ou=accounts,o=redbrick" attrs=cn
         by dn.regex="cn=root,ou=ldap,o=redbrick" write
         by self read
         by * none
 
-			access to attrs=userPassword
+      access to attrs=yearsPaid,year,course,id,newbie,altmail
+        by dn.regex="cn=root,ou=ldap,o=redbrick" write
+        by self read
+        by * none
+
+      access to attrs=userPassword
         by dn.regex="cn=root,ou=ldap,o=redbrick" write continue
         by self write
         by anonymous auth
         by * none
 
-			access to attrs=gecos,loginShell
+      access to attrs=gecos,loginShell
         by dn.regex="cn=root,ou=ldap,o=redbrick" write continue
         by self write
         by * read
 
-			# Default ACL
-			access to *
+      # Default ACL
+      access to *
         by * read
-		'';
-	};
+    '';
+  };
 }
