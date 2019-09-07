8c1df51382a3d92814b1fedf4ecd43a741e175fb
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-09-07 23:48:15 +0100

    Move ldap extra option to own file

--- services/ldap/default.nix
+++ services/ldap/default.nix
@@ -8,13 +8,6 @@ let
   rootpwFile = "/var/secrets/ldap.secret";
   slurpdpwFile = "/var/secrets/slurpd.secret";
 in {
-  options.redbrick.ldapSlaveTo = lib.mkOption {
-    description = "If this host is going to be an LDAP slave, set this to a hostname";
-    default = null;
-    defaultText = "Null (this is a master)";
-    type = types.nullOr types.str;
-  };
-
   services.openldap = {
     inherit rootpwFile;
     enable = true;
