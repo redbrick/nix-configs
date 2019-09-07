4734c50db04999f4e23bddca38f3112537c88251
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-09-07 23:43:39 +0100

    Fix typo

--- services/ldap/default.nix
+++ services/ldap/default.nix
@@ -8,7 +8,7 @@ let
   rootpwFile = "/var/secrets/ldap.secret";
   slurpdpwFile = "/var/secrets/slurpd.secret";
 in {
-  options.redbrick.ldapSlaveTo = mkOption {
+  options.redbrick.ldapSlaveTo = lib.mkOption {
     description = "If this host is going to be an LDAP slave, set this to a hostname";
     default = null;
     defaultText = "Null (this is a master)";
