f5504f2acfa0137bb1c590679411913d7c1b8376
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-10-20 01:32:09 +0100

    Simplify zfsquota service

--- services/zfsquota/default.nix
+++ services/zfsquota/default.nix
@@ -2,13 +2,6 @@
 let
   common = import ../../common/variables.nix;
   zfsPackage = if config.boot.zfs.enableUnstable then pkgs.zfsUnstable else pkgs.zfs;
-  scriptName = "zfsquota.sh";
-
-  quotaScript = pkgs.writeShellScriptBin scriptName ''
-    ldapsearch -h ${common.ldapHost} -p 389 -xLLL -b o=redbrick objectClass=posixAccount uidNumber |\
-    awk '/^uidNumber/ { print "userquota@"$2"=${config.redbrick.zfsquotaSize}" }' |\
-    xargs -I {} zfs set {} ${config.redbrick.zfsquotaDataset}
-  '';
 in {
   imports = [ ./options.nix ];
 
@@ -18,16 +11,21 @@ in {
 
     path = with pkgs; [ openldap zfsPackage gawk ];
 
+    script = ''
+      ldapsearch -h ${common.ldapHost} -p 389 -xLLL -b o=redbrick objectClass=posixAccount uidNumber |\
+      awk '/^uidNumber/ { print "userquota@"$2"=${config.redbrick.zfsquotaSize}" }' |\
+      xargs -I {} zfs set {} ${config.redbrick.zfsquotaDataset}
+    '';
+
     serviceConfig = {
-      ExecStart = "${quotaScript}/bin/${scriptName}";
       Restart = "no";
+      Type = "oneshot";
     };
   };
 
   systemd.timers.zfsquota = {
     description = "Timer to kick off zfsquota.service every day at 8am";
     wantedBy = [ "multi-user.target" ];
-    restartTriggers = [ quotaScript ];
 
     timerConfig = {
       OnCalendar = "08:00:00";
