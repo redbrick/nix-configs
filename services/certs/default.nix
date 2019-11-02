20e0f512e4707c609a39b1d198b4ec3495774165
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-11-02 16:55:23 +0000

    Add ignore propagation flag to LE

--- services/certs/default.nix
+++ services/certs/default.nix
@@ -8,7 +8,7 @@ in {
   security.acme = {
     legoCerts."${common.tld}" = {
       email = "admins+acme@${common.tld}";
-      dnsProvider = "rfc2136";
+      dnsProvider = "rfc2136 --dns.disable-cp"; # Adding an arg like this is a bit of a hack
       credentialsFile = "/var/secrets/certs.secret";
       extraDomains."*.${common.tld}" = null;
     };
