ca8faa9077ece5a98e40404b0f879569c2f60ef4
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-11-25 22:31:17 +0000

    Add butlerxvm config

---
+++ hosts/butlerxvm/configuration.nix
@@ -0,0 +1,28 @@
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../common/sysconfig.nix
+    ../../services/ssh.nix
+  ];
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.03";
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "/dev/vda";
+
+  networking = {
+    hostName = "butlerxvm";
+    hostId = "6df4f83d";
+    defaultGateway = "192.168.0.254";
+    interfaces.enp1s0.ipv4.addresses = [{
+      address = "192.168.0.136";
+      prefixLength = 24;
+    }];
+  };
+}
