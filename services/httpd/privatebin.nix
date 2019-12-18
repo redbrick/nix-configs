702bdd03248eb9f1ac1d203c374eedf2fe419c0f
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-12-18 21:59:00 +0000

    add privatebin

---
+++ services/httpd/privatebin.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+with (import ./shared.nix);
+{
+  services.httpd.virtualHosts = [
+    (vhost {
+      user = "wiki";
+      group = "redbrick;
+      hostName = "paste.${tld}";
+      serverAliases = [];
+      documentRoot = import ../../packages/privatebin {inherit pkgs;};
+    })
+  ];
+}
