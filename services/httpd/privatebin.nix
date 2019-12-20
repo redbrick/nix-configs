cae89bc718c96182cc2e80d4571822369de93ea0
James McDermott <james.mcdermott7@mail.dcu.ie>
2019-12-20 13:10:55 +0000

    add system tmpfile rule and relative import for php config

--- services/httpd/privatebin.nix
+++ services/httpd/privatebin.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }:
 with (import ./shared.nix);
-{
+let
+  user = "wiki";
+  group = "redbrick";
+in {
   services.httpd.virtualHosts = [
     (vhost {
-      user = "wiki";
-      group = "redbrick";
+      user = "${user}";
+      group = "${group}";
       hostName = "paste.${tld}";
       serverAliases = [];
       documentRoot = import ../../packages/privatebin {inherit pkgs;};
-      extraConfig = "SetEnv CONFIG_PATH /var/lib/privatebin/conf.php";
+      extraConfig = "SetEnv CONFIG_PATH ${./conf.php}";
     })
+  ];
+  systemd.tmpfiles.rules = [
+    "d '/var/lib/privatebin' 0750 ${user} ${group} - -"
   ];
 }
