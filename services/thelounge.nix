d631aaa1dec9bba67189a2f22e7f417d06c53e41
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-12-22 19:46:14 +0000

    Add oidentd config to thelounge

--- services/thelounge.nix
+++ services/thelounge.nix
@@ -1,6 +1,16 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 let
     ldapAccount = import /var/secrets/thelounge.nix;
+    identPort = 1113;
+    oidentdConfig = pkgs.writeText "oident.conf" ''
+      user "thelounge" {
+        default {
+          allow spoof
+          allow spoof_all
+          force forward 127.0.0.1 ${builtins.toString identPort}
+        }
+      }
+    '';
 in {
     services.thelounge = {
         enable = true;
@@ -16,6 +26,10 @@ in {
                 password = "";
                 join = "#lobby,#intersocs,#bots,#helpdesk";
             };
+            identd = {
+                enable = true;
+                port = identPort;
+            };
             ldap = {
                 enable = true;
                 url = config.users.ldap.server;
@@ -29,4 +43,14 @@ in {
             };
         };
     };
+
+    systemd.services.oidentd = {
+      description = "identd server, routing requests to TheLounge";
+      wantedBy = [ "multi-users.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.oidentd}/bin/oidentd -c ${oidentdConfig} -i -S";
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 113 ];
 }
