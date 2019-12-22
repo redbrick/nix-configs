e30306a19eccad916f8098ab5c966ff9386494d5
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2019-12-22 20:17:51 +0000

    Add tunnel svc to zeus for irc

--- services/thelounge.nix
+++ services/thelounge.nix
@@ -1,5 +1,6 @@
 { pkgs, config, ... }:
 let
+    identityFile = "/var/secrets/id_ed25519";
     ldapAccount = import /var/secrets/thelounge.nix;
     identPort = 1113;
     oidentdConfig = pkgs.writeText "oident.conf" ''
@@ -46,9 +47,27 @@ in {
 
     systemd.services.oidentd = {
       description = "identd server, routing requests to TheLounge";
-      wantedBy = [ "multi-users.target" ];
+      wantedBy = [ "multi-user.target" ];
       serviceConfig = {
         ExecStart = "${pkgs.oidentd}/bin/oidentd -c ${oidentdConfig} -i -S";
+      };
+    };
+
+    # TODO remove when ircd-hybrid is listening on a 192.168.0.0/24 address
+    systemd.services.irc-tunnel = {
+      description = "IRC tunnel";
+      before = [ "thelounge.service" ];
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      serviceConfig = {
+          ExecStart = ''
+            ${pkgs.openssh}/bin/ssh -o 'UserKnownHostsFile=/dev/null' -o 'StrictHostKeyChecking=no' \
+            -i ${identityFile} -vNL 6667:irc.redbrick.dcu.ie:6667 portfwd@zeus.internal
+          '';
+          Restart = "always";
+          RestartSec = "10";
+          WorkingDirectory = "/var/empty";
       };
     };
 
