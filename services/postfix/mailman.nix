b3c5e1ccebe613c3a6f0ac099c9bf33b457610a9
Lucas Savva <lucas@m1cr0man.com>
2020-02-14 08:04:29 +0000

    Remove mailman overlays

--- services/postfix/mailman.nix
+++ services/postfix/mailman.nix
@@ -3,34 +3,6 @@ let
   tld = config.redbrick.tld;
   hyperkittySecret = "/var/secrets/hyperkitty.secret";
 in {
-  # https://github.com/NixOS/nixpkgs/issues/44426
-  # needs to come before all other in alphabetical order (or make use of
-  # lib.mkBefore)
-  # from https://git.immae.eu/?p=perso/Immae/Config/Nix.git;a=blob;f=overlays/python-packages/default.nix;h=0feff55eea2b5ea220cb135cd1e134a713f0fcca;hb=HEAD
-  nixpkgs.overlays = [
-    (self: super: let
-      pyNames = [ "python37" ];
-      overriddenPython = name: [
-        { inherit name; value = super.${name}.override { packageOverrides = pyself: pysuper: {
-            hyperkitty = pysuper.hyperkitty.overrideAttrs (oldAttrs: rec {
-              version = "1.3.2";
-              # TODO fix undefined fetchPypi
-              src = self.fetchPypi {
-                inherit version;
-                pname = "HyperKitty";
-                sha256 = "wat";
-              };
-            });
-          };
-        }; }
-        { name = "${name}Packages"; value = self.recurseIntoAttrs self.${name}.pkgs; }
-      ];
-      overriddenPythons = builtins.concatLists (map overriddenPython pyNames);
-    in {
-      buildPythonOverrides = newOverrides: currentOverrides: super.lib.composeExtensions newOverrides currentOverrides;
-    } // super.lib.attrsets.listToAttrs overriddenPythons)
-  ];
-
   services.mailman = {
     enable = true;
     siteOwner = "postmaster@${tld}";
