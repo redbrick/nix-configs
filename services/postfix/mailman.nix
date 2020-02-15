5da15df02114b8981bd7543595da447d4597c9fe
Lucas Savva <lucas@m1cr0man.com>
2020-02-15 18:39:04 +0000

    Add missing mailman postfix fields

--- services/postfix/mailman.nix
+++ services/postfix/mailman.nix
@@ -7,7 +7,12 @@ in {
     enable = true;
     siteOwner = "postmaster@${tld}";
     webHosts = [ "0.0.0.0" ];
-    hyperkittyApiKey = lib.fileContents hyperkittySecret;
+    hyperkitty.enable = true;
+  };
+  services.postfix = {
+    relayDomains = [ "hash:/var/lib/mailman/data/postfix_domains" ];
+    config.transport_maps = [ "hash:/var/lib/mailman/data/postfix_lmtp" ];
+    config.local_recipient_maps = [ "hash:/var/lib/mailman/data/postfix_lmtp" ];
   };
   networking.firewall.allowedTCPPorts = [ 80 ];
 }
