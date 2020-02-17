8b5fb2670e81b718b7284496184ffeaeb367c77f
butlerx <butlerx@notthe.cloud>
2020-02-17 21:53:02 +0000

    fix no proxy rules

--- common/sysconfig.nix
+++ common/sysconfig.nix
@@ -24,7 +24,7 @@ in {
   networking.nameservers = ["192.168.0.4"];
   networking.timeServers = ["192.168.0.254"];
   networking.proxy.default = "http://proxy.internal:3128/";
-  networking.proxy.noProxy = "127.0.0.1,localhost,*.internal";
+  networking.proxy.noProxy = "127.0.0.1,localhost,192.168.0,.internal";
 
   # Enable rsyslog
   services.rsyslogd.enable = true;
