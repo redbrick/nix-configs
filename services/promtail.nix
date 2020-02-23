c138f5b29f7d3900de7f3b3dc4fbd41e6556f438
butlerx <butlerx@notthe.cloud>
2020-02-23 15:59:24 +0000

    add metric collectors for logs

--- services/promtail.nix
+++ services/promtail.nix
@@ -6,83 +6,82 @@ let
       echo '${builtins.toJSON conf}' | ${pkgs.jq}/bin/jq 'del(._module)' > $out
     '';
   dataDir = "/var/lib/promtail";
+  syslog = {
+    job_name = "syslog";
+    syslog = {
+      listen_address = "0.0.0.0:514";
+      idle_timeout = "60s";
+      label_structured_data = true;
+      labels = {
+        job = "syslog";
+      };
+    };
+    relabel_configs = [
+      {
+        source_labels = [ "__syslog_message_hostname" ];
+        target_label = "host";
+      }
+      {
+        source_labels = [ "__syslog_connection_ip_address" ];
+        target_label = "ip";
+      }
+      {
+        source_labels = ["__syslog_message_app_name"];
+        target_label = "app";
+      }
+      {
+        source_labels = ["__syslog_message_severity"];
+        target_label = "level";
+      }
+    ];
+  };
+  journal = {
+    job_name = "journal";
+    journal = {
+      max_age= "12h";
+      labels = {
+        job = "systemd-journal";
+      };
+    };
+    relabel_configs = [
+      {
+        source_labels = [ "__journal__systemd_unit" ];
+        target_label = "unit";
+      }
+      {
+        source_labels = [ "__journal__hostname" ];
+        target_label = "host";
+      }
+      {
+        source_labels = ["__journal_syslog_identifier"];
+        target_label = "service";
+      }
+      {
+        source_labels = ["__journal_errno"];
+        target_label = "severity";
+      }
+      {
+        source_labels = ["__journal_priority"];
+        target_label = "level";
+      }
+      {
+        source_labels = ["__journal__UID"];
+        target_label = "uid";
+      }
+      {
+        source_labels = ["__journal__GID"];
+        target_label = "gid";
+      }
+    ];
+  };
   configuration = {
     server = {
       http_listen_port = 9080;
     };
     clients = [{
-      url = "http://hardcase.internal:3100/loki/api/v1/push";
+      url = "http://log.internal:3100/loki/api/v1/push";
     }];
-    scrape_configs = [
-      {
-        job_name = "syslog";
-        syslog = {
-          listen_address = "0.0.0.0:514";
-          idle_timeout = "60s";
-          label_structured_data = true;
-          labels = {
-            job = "syslog";
-          };
-        };
-        relabel_configs = [
-          {
-            source_labels = [ "__syslog_message_hostname" ];
-            target_label = "host";
-          }
-          {
-            source_labels = [ "__syslog_connection_ip_address" ];
-            target_label = "ip";
-          }
-          {
-            source_labels = ["__syslog_message_app_name"];
-            target_label = "app";
-          }
-          {
-            source_labels = ["__syslog_message_severity"];
-            target_label = "level";
-          }
-        ];
-      }
-      # {
-      #   job_name = "journal";
-      #   journal = {
-      #     max_age= "12h";
-      #     labels = {
-      #       job = "systemd-journal";
-      #     };
-      #   };
-      #   relabel_configs = [
-      #     {
-      #       source_labels = [ "__journal__systemd_unit" ];
-      #       target_label = "unit";
-      #     }
-      #     {
-      #       source_labels = [ "__journal__hostname" ];
-      #       target_label = "host";
-      #     }
-      #     {
-      #       source_labels = ["__journal_syslog_identifier"];
-      #       target_label = "service";
-      #     }
-      #     {
-      #       source_labels = ["__journal_errno"];
-      #       target_label = "severity";
-      #     }
-      #     {
-      #       source_labels = ["__journal_priority"];
-      #       target_label = "level";
-      #     }
-      #     {
-      #       source_labels = ["__journal__UID"];
-      #       target_label = "uid";
-      #     }
-      #     {
-      #       source_labels = ["__journal__GID"];
-      #       target_label = "gid";
-      #     }
-      #   ];
-      # }
-    ];
+    scrape_configs = [ syslog ];
   };
 in {
   systemd.services.promtail = {
