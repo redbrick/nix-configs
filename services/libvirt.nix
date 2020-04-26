3452ef6a443db95ace94af66f6e7fa2a827c245d
Lucas Savva <lucas@m1cr0man.com>
2020-04-26 11:01:49 +0100

    Remove cert + tls listener from libvirt
    Can be connected to via SSH just fine

--- services/libvirt.nix
+++ services/libvirt.nix
@@ -1,23 +1,6 @@
-{ config, ... }:
-let
-  tld = config.redbrick.tld;
-in {
-
-  # Need a cert specifically for the listening port
-  # It's hard to get libvirt to work without tls
-  security.acme.acceptTerms = true;
-  security.acme.certs.libvirt = {
-    domain = "${config.networking.hostName}.${tld}";
-    email = "webmaster+acmelibvirt@${tld}";
-    dnsProvider = "rfc2136";
-    credentialsFile = "/var/secrets/certs.secret";
-    dnsPropagationCheck = false;
-  };
-
+{
   virtualisation.libvirtd = {
     enable = true;
     onShutdown = "shutdown";
-    extraConfig = "listen_tls = 0\nlisten_tcp = 1";
-    extraOptions = ["-l"];
   };
 }
