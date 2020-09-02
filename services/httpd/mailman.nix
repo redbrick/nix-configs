9db4bfb39edd36f686e19cfe821fd39ca066423a
Lucas Savva <lucas@m1cr0man.com>
2020-09-02 21:57:53 +0100

    Fix mailman httpd config

--- services/httpd/mailman.nix
+++ services/httpd/mailman.nix
@@ -7,9 +7,21 @@ let
   staticRoot = "/var/lib/mailman-web-static";
   proxyAddress = "unix:/run/mailman-web.socket|uwsgi://127.0.0.1/";
 
-  vhostConfig = (vhostProxy proxyAddress) // {
+  vhostConfig = (vhostCerts tld) // {
+    inherit adminAddr;
+    onlySSL = true;
     serverAliases = [ "localmail.${tld}" ];
     servedDirs = [ { dir = staticRoot; urlPath = "/static"; } ];
+    extraConfig = ''
+      <Proxy *>
+        Order deny,allow
+        Allow from all
+      </Proxy>
+
+      ProxyPassMatch ^/static !
+      ProxyPass / ${proxyAddress}/
+      ProxyPassReverse / ${proxyAddress}/
+    '';
   };
 in {
   # Serve tries to enable nginx. Force it off.
@@ -17,5 +29,5 @@ in {
   services.nginx.enable = false;
 
   services.mailman.webUser = config.services.httpd.user;
-  services.httpd.virtualHosts."lists.${tld}" = vhostConfig // (vhostCerts tld);
+  services.httpd.virtualHosts."lists.${tld}" = vhostConfig;
 }
