de114ab010fe8243d6fd9236584521e9d085774a
Lucas Savva <lucas@m1cr0man.com>
2020-09-22 19:49:05 +0100

    Add bonding to metharme

--- hosts/metharme/configuration.nix
+++ hosts/metharme/configuration.nix
@@ -12,7 +12,7 @@ in {
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "20.09";
+  system.stateVersion = "21.03";
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
@@ -23,9 +23,5 @@ in {
     hostName = "metharme";
     hostId = "1f03060e";
     defaultGateway = "192.168.0.254";
-    interfaces.eno2.ipv4.addresses = [{
-      address = "192.168.0.156";
-      prefixLength = 24;
-    }];
-  };
+  } // (variables.bondConfig [ "eno1" "eno2" ] "192.168.0.156");
 }
