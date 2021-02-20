902fa1312222199dbbfafb970179071eddc6e48d
Cian Butler <butlerx@notthe.cloud>
2021-02-20 11:30:40 +0000

    Update go-bridge shas

--- packages/go-discord-irc/metadata.nix
+++ packages/go-discord-irc/metadata.nix
@@ -1,5 +1,5 @@
 {
-  vendorSha256 = "1xfspsxsb9if3lc51fgcbxr0ab9127rdw3hgm8bimcd1cds0a4l2";
+  vendorSha256 = "1l505jjc8pbgjppb8wl745dl1kwgwhrsgy7d2ayyyb45czk69r3w";
   rev = "e810d6b2109825a0e15f4cd01690846c1b75a147";
-  sha256 = "1dn6m61rcii0jps1bhacahzkzx4mssgjxjw9hgyw5vw5gs1mj8wq";
+  sha256 = "1n9g1siqid79h7138qb63jnmjvgjvy8wfjs9p8n72qlcvy0frhpm";
 }
