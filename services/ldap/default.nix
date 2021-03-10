7a0766d88357a14227dbe7d335f823bd8023cfc8
Lucas Savva <m1cr0man@redbrick.dcu.ie>
2021-03-10 00:24:09 +0000

    Convert schema files to LDIF format

--- services/ldap/default.nix
+++ services/ldap/default.nix
@@ -10,12 +10,7 @@ let
   dbDirectory = "/var/db/openldap";
 in {
   services.openldap = {
-    inherit rootpwFile;
     enable = true;
-    suffix = "o=redbrick";
-    rootdn = "cn=root,ou=ldap,o=redbrick";
-    defaultSchemas = false; # We don't use the nis.schema
-    database = "hdb";
 
     settings = {
       attrs = {
@@ -25,12 +20,12 @@ in {
       };
       children = {
         "cn=schema".includes = [
-          "${pkgs.openldap.out}/etc/schema/core.schema"
-          "${pkgs.openldap.out}/etc/schema/cosine.schema"
-          "${pkgs.openldap.out}/etc/schema/inetorgperson.schema"
-          "${./schema/common.schema}"
-          "${./schema/system.schema}"
-          "${./schema/userdb.schema}"
+          "${pkgs.openldap.out}/etc/schema/core.ldif"
+          "${pkgs.openldap.out}/etc/schema/cosine.ldif"
+          "${pkgs.openldap.out}/etc/schema/inetorgperson.ldif"
+          "${./schema/common.ldif}"
+          "${./schema/system.ldif}"
+          "${./schema/userdb.ldif}"
         ];
         "olcDatabase={-1}frontend".attrs = {
           objectClass = [ "olcDatabaseConfig" "olcFrontendConfig" ];
@@ -65,92 +60,21 @@ in {
             olcRootPW = {
               path = rootpwFile;
             };
-          };
+          } // (lib.optionalAttrs (config.redbrick.ldapSlaveTo != null) {
+              olcSyncrepl = "rid=000 provider=ldap://${config.redbrick.ldapSlaveTo}:389 bindmethod=simple timeout=0 network-timeout=0 binddn=\"cn=slurpd,ou=ldap,o=redbrick\" credentials=\"${lib.fileContents slurpdpwFile}\" keepalive=0:0:0 starttls=no filter=\"(objectclass=*)\" searchbase=\"o=redbrick\" scope=sub attrs=\"*,+\" schemachecking=off type=refreshAndPersist retry=\"5 5 300 +\"";
+              olcMirrorMode = "FALSE";
+          });
           children = {
             # TODO test + repl config
-            "olcOverlay={0}syncprov".attrs = if (config.redbrick.ldapSlaveTo == null) then {
+            "olcOverlay={0}syncprov".attrs = lib.optionalAttrs (config.redbrick.ldapSlaveTo == null) {
               objectClass = [ "olcOverlayConfig" "olcSyncProvConfig" ];
               olcOverlay = "{0}syncprov";
               olcSpCheckpoint = "100 10";
-              olcAccess = [ "{0}to * by * read break" ];
-            } else {
-              olcSyncrepl = "rid=000 provider=ldap://${config.redbrick.ldapSlaveTo}:389 bindmethod=simple timeout=0 network-timeout=0 binddn=\"cn=slurpd,ou=ldap,o=redbrick\" credentials=\"${lib.fileContents slurpdpwFile}\" keepalive=0:0:0 starttls=no filter=\"(objectclass=*)\" searchbase=\"o=redbrick\" scope=sub attrs=\"*,+\" schemachecking=off type=refreshAndPersist retry=\"5 5 300 +\"";
-              olcMirrorMode = "FALSE";
             };
           };
+        };
       };
     };
-
-    extraDatabaseConfig = ''
-      cachesize 100000
-    '' + (if (config.redbrick.ldapSlaveTo == null) then ''
-
-      # Master config
-      overlay syncprov
-      syncprov-checkpoint 100 10
-    '' else ''
-      syncrepl rid=000
-        provider=ldap://${config.redbrick.ldapSlaveTo}:389
-        type=refreshAndPersist
-        retry="5 5 300 +"
-        attrs="*,+"
-        binddn="cn=slurpd,ou=ldap,o=redbrick"
-        bindmethod=simple
-        credentials=${lib.fileContents slurpdpwFile}
-        searchbase="o=redbrick"
-    '');
-    extraConfig = ''
-      include ${pkgs.openldap.out}/etc/schema/core.schema
-      include ${pkgs.openldap.out}/etc/schema/cosine.schema
-      include ${pkgs.openldap.out}/etc/schema/inetorgperson.schema
-      include ${./schema/common.schema}
-      include ${./schema/system.schema}
-      include ${./schema/userdb.schema}
-
-      backend hdb
-      lastmod on
-
-      sizelimit unlimited
-      loglevel ${config.services.openldap.logLevel}
-
-      # ACLs
-      access to dn.children="ou=2002,ou=accounts,o=redbrick"
-        by dn.regex="cn=root,ou=ldap,o=redbrick" write
-        by dn.regex="cn=slurpd,ou=ldap,o=redbrick" read
-        by * none
-
-      access to dn.children="ou=accounts,o=redbrick" attrs=cn
-        by dn.regex="cn=root,ou=ldap,o=redbrick" write
-        by dn.regex="cn=slurpd,ou=ldap,o=redbrick" read
-        by dn.regex="cn=mediawiki,ou=reserved,o=redbrick" read
-        by self read
-        by * none
-
-      access to attrs=yearsPaid,year,course,id,newbie,altmail
-        by dn.regex="cn=root,ou=ldap,o=redbrick" write
-        by dn.regex="cn=slurpd,ou=ldap,o=redbrick" read
-        by dn.regex="cn=mediawiki,ou=reserved,o=redbrick" read
-        by self read
-        by * none
-
-      access to attrs=userPassword
-        by dn.regex="cn=root,ou=ldap,o=redbrick" write continue
-        by dn.regex="cn=slurpd,ou=ldap,o=redbrick" read
-        by dn.regex="cn=dovecot,ou=reserved,o=redbrick" read
-        by self write
-        by anonymous auth
-        by * none
-
-      access to attrs=gecos,loginShell
-        by dn.regex="cn=root,ou=ldap,o=redbrick" write continue
-        by dn.regex="cn=slurpd,ou=ldap,o=redbrick" read
-        by self write
-        by * read
-
-      # Default ACL
-      access to *
-        by * read
-    '';
   };
 
   networking.firewall.allowedTCPPorts = [ 389 ];
