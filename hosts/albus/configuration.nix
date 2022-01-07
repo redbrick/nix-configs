1c73449fb46cc6938e65237ea4b83f8ed1273391
System administrator <root@albus.redbrick.dcu.ie>
2022-01-07 00:01:04 +0000

    remove vm configuration

--- hosts/albus/configuration.nix
+++ hosts/albus/configuration.nix
@@ -43,8 +43,6 @@ in {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHM+HZVUPkFX/Xlxla/2JYpFrt2vEG2nJVcjRpJhGU3c root@daedalus"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGjw3ENwy/fBX6EOqwppSv1c0m5buvKE8OaS810BTaFo root@icarus"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOLnjvcD8MnRKf5YDvKqDWK+kGMfYhSb9l54jW8nd0H1 root@m1cr0man"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBvBThPI3TmXVOKrY39l0rFLOdaLl0a0Xh/IaRQ+tAA+ root@butlerxvm"
     ];
   };
 
