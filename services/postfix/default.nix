8b9d9779d9c3c6049d331c73598cd2b5d8ea1b39
James Hackett <jamesthackett1@gmail.com>
2023-05-21 16:15:51 +0100

    Update default.nix

--- services/postfix/default.nix
+++ services/postfix/default.nix
@@ -87,19 +87,20 @@ in {
     systemctl restart postfix
   '';
   
-  environment.etc."postfix/header_checks".text = ''
+  environment.etc = {
+  "postfix/header_checks".text = ''
     /^Received: +from +(redbrick\.dcu\.ie) +/
         reject forged client name in Received: header: $1
     /^Received: +from +[^ ]+ +\(([^ ]+ +[he]+lo=|[he]+lo +)(redbrick\.dcu\.ie)\)/
         reject forged client name in Received: header: $2
   '';
-  
-  environment.etc."postfix/body_checks".text = ''
+  "postfix/body_checks".text = ''
     /^[> ]*Received: +from +(redbrick\.\dcu\.ie) /
         reject forged client name in Received: header: $1
     /^[> ]*Received: +from +[^ ]+ +\(([^ ]+ +[he]+lo=|[he]+lo +)(redbrick\.dcu\.ie)\)/
         reject forged client name in Received: header: $2
   '';
+  };
 
   services.postfix = {
     enable = true;
